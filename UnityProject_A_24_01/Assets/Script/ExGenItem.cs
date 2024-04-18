00e3514fbe2e97134ace455cdc811271215daa1b
gamja54 <gghhjj867@gmail.com>
2024-04-18 15:47:53 +0900

    [Feat] DoTween Scene 제작 연출

--- UnityProject_A_24_01/Assets/Script/ExGenItem.cs
+++ UnityProject_A_24_01/Assets/Script/ExGenItem.cs
@@ -13,11 +13,11 @@ public class ExGenItem : MonoBehaviour
         checkTime += Time.deltaTime;                                                 //프레임 시간을 쌓아서 초를 검사한다.
         if(checkTime >= 2.0f)                                                        //2초의 시간이 흐르면
         {
-            checkTime = 0.0f;
-            GameObject temp = Instantiate(ItemBox);                                  //시간 초기화를 시킨다.
-            temp.transform.position = this.gameObject.transform.position;
-            int RandomNumber = Random.Range(0, 4);
-            temp.transform.position += new Vector3(0.0f, RandomNumber, 0.0f);
+            checkTime = 0.0f;                                                        //시간 초기화를 시킨다.
+            GameObject temp = Instantiate(ItemBox);                                  //아이템박스 프리팹을 Instantiate로 생성한다.
+            temp.transform.position = this.gameObject.transform.position;            //생성할때 스크립트가 있는 오브젝트 위치로 생성
+            int RandomNumber = Random.Range(0, 4);                                   //0에 3까지의 랜덤값을 받아서
+            temp.transform.position += new Vector3(0.0f, RandomNumber, 0.0f);        //Y값 위치에 더해준다.
         }
         
     }
