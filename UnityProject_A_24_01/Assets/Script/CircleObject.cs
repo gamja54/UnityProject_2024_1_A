35dd089d718d2fc68396cded8f42d1991852573c
gamja54 <gghhjj867@gmail.com>
2024-05-16 14:44:48 +0900

    [Feat] 과일 Merge 까지

--- UnityProject_A_24_01/Assets/Script/CircleObject.cs
+++ UnityProject_A_24_01/Assets/Script/CircleObject.cs
@@ -8,12 +8,19 @@ public class CircleObject : MonoBehaviour
     public bool isUsed;           //사용 완료 체크
     Rigidbody2D rigidbody2D;      //2D 강체 선언
 
-    // Start is called before the first frame update
-    void Start()
+    public int index;             //과일 번호 설정
+
+    private void Awake()
     {
         isUsed = false;                              //시작할 때 사용이 안되었다고 입력
         rigidbody2D = GetComponent<Rigidbody2D>();   //오브젝트의 강체에 접근
         rigidbody2D.simulated = false;               //물리 행동이 처음에는 동작하기 않게 설정
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
     }
 
     // Update is called once per frame
@@ -57,6 +64,38 @@ public class CircleObject : MonoBehaviour
         if(temp != null)                                            //해당 오브젝트가 있을 경우
         {
             temp.gameObject.GetComponent<GameManager>().GenObject();  //GameManager의 GenObject 함수를 호출
+        }
+    }
+
+    public void Used()
+    {
+        isDrag = false;
+        isUsed = true;
+        rigidbody2D.simulated = true;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)                            //해당 오브젝트가 충돌 했을때 OnCollisionEnter2D
+    {
+        if (index >= 7)
+            return;
+
+        if (collision.gameObject.tag == "Fruit")
+        {
+            CircleObject temp = collision.gameObject.GetComponent<CircleObject>();  //충돌한 물체에서 같은 Class를 받아온다.    
+            if(temp.index == index)                                           //충돌 index와 내 index가 같다
+            {
+                if(gameObject.GetInstanceID() > collision.gameObject.GetInstanceID())   //2개 합쳐서 1개를 만들기 위해서 ID검사 후 큰것만
+                {
+                    //GameManager에서 합친 오브젝트를 생성 
+                    GameObject tempGameManager = GameObject.FindWithTag("GameManager");
+                    if (tempGameManager != null)
+                    {
+                        tempGameManager.gameObject.GetComponent<GameManager>().MergeObject(index, gameObject.transform.position);
+                    }
+                    Destroy(temp.gameObject);     //충돌한 물체 제거
+                    Destroy(gameObject);          //자신도 제거
+                }
+            }
         }
     }
 }
--- UnityProject_A_24_01/Assets/Script/GameManager.cs
+++ UnityProject_A_24_01/Assets/Script/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject CircleObject;
+    public GameObject[] CircleObject;
     public Transform genTransform;
     public float timeCheck;
     public bool isGen;
@@ -24,15 +24,23 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isGen == false)                                       //isSen 플래그가 false일 경우
+        if(isGen == false)                                         //isGen 플래그가 false일 경우
         {
-            timeCheck -= Time.deltaTime;                         //매 프레임 돌아가면서 시간을 감소 시킨다.
-            if(timeCheck <= 0.0f)                                //0초 이하가 되었을 경우
+            timeCheck -= Time.deltaTime;                          //매 프레임 돌아가면서 시간을 감소 시킨다.
+            if(timeCheck <= 0.0f)                                 //0초 이하가 되었을 경우
             {
-                GameObject Temp = Instantiate(CircleObject);     //프리팹 생성 후 Temp 오브젝트에 넣는다.
-                Temp.transform.position = genTransform.position; //고정 위치에 생성시킨다.s
+                int RandNumber = Random.Range(0, 3);                       //0 ~ 2 의 랜덤 넘버 생성
+                GameObject Temp = Instantiate(CircleObject[RandNumber]);   //프리팹 생성 후 Temp 오브젝트에 넣는다.
+                Temp.transform.position = genTransform.position;  //고정 위치에 생성시킨다.
                 isGen = true;
             }
         }
+    }
+
+    public void MergeObject(int index, Vector3 position)
+    {
+        GameObject Temp = Instantiate(CircleObject[index]);
+        Temp.transform.position = position;
+        Temp.GetComponent<CircleObject>().Used();
     }
 }
